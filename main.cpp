f8e135efb06b19feb486b383bd99ad6dc48b41fa
ManuNunez <niuweme.nunez@gmail.com>
2025-09-11 17:45:40 -0600

    file : welcome

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	cout<<endl<<endl;
+	cout<<"-----------------------------------------------"<<endl<<endl;
+	cout<<"BIENVENIDOS AL CLUB DE ALGORITMIA DEL CUCHAPALA"<<endl<<endl;
+	cout<<"-----------------------------------------------"<<endl<<endl;
+	return 0;
+}
